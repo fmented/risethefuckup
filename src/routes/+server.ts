d0cdff4a8ac1eb42511fe7711662eb48c4d48db9
fmented <fmented@gmail.com>
2023-11-13 05:21:40 +0700

    y

--- src/routes/+server.ts
+++ src/routes/+server.ts
@@ -1,17 +1,25 @@
 import type { RequestHandler } from '@sveltejs/kit';
-
+import cookie from "cookie"
 export const POST: RequestHandler = async ({ cookies, request, }) => {
 
     try {
-        const passcode = request.headers.get("passcode")
+        const passcode = request.headers.get("passcode") || ""
         if (passcode === import.meta.env.VITE_PASSCODE) {
             cookies.set("passcode", passcode!, {
                 path: "/",
             })
-            return new Response(null, {
+
+            const r = new Response(null, {
                 status: 301,
-                headers: new Headers({ Location: "/check-in" })
+                headers: new Headers({
+                    Location: "/check-in", "Set-Cookie": cookie.serialize("passcode", passcode, {
+                        httpOnly: true,
+                        maxAge: 60 * 60 * 24 * 30
+
+                    })
+                })
             })
+            return r
         }
         return new Response(JSON.stringify({ status: import.meta.env.VITE_PASSCODE }));
 
